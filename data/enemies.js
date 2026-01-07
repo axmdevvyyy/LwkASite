09a364c44c927af53df98293725632210bf78cd9
axmdevvyyy <a.nunez3@share.epsb.ca>
2026-01-07 15:02:39 -0700

    Add ENEMIES array with enemy definitions

---
+++ data/enemies.js
@@ -0,0 +1,7 @@
+export const ENEMIES = [
+  { id: 'grunt_melee', name: 'Grunt', type: 'melee', hp: 20, damage: 6 },
+  { id: 'grunt_ranged', name: 'Lookout', type: 'ranged', hp: 12, damage: 4 },
+  { id: 'elite_shield', name: 'Shielded Elite', type: 'elite', hp: 60, damage: 12, special: 'shield' },
+  // Bosses are defined with phases elsewhere (MVP sample)
+  { id: 'neon_colossus', name: 'Neon Colossus', type: 'boss', hp: 500, damage: 30 }
+];
